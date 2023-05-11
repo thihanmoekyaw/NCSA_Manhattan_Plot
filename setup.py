35056311da7f55a73dc70c800860a87baa20e09a
kkaung66 <kkaung66@gmail.com>
2023-05-11 13:15:34 -0500

    bug fixes

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='NCSAMahanttanPlot',
+    name='NCSAManhattanPlot',
     version='0.1',
     author='NCSA Visual Analytics',
     url='https://github.com/thihanmoekyaw/NCSA_Manhattan_Plot',
